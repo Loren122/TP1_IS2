583be357a8bea9ae080ca792bf5f71cf81d168dd
Lorenzo Paredes <lorenzoparedes122@gmail.com>
2024-03-27 18:17:47 -0300

    Modificacion de factorial.py para que tenga un rango como parametro

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -6,23 +6,27 @@
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
 import sys
-def factorial(num): 
-    if num < 0: 
-        print("Factorial de un número negativo no existe")
 
-    elif num == 0: 
-        return 1
-        
-    else: 
-        fact = 1
-        while(num > 1): 
-            fact *= num 
-            num -= 1
-        return fact 
+def factorial(desde, hasta): 
+    for num in range(desde, hasta+1):
+        if num < 0: 
+            print("Factorial de un número negativo no existe")
+        elif num == 0: 
+            print("Factorial de 0 es 1")
+        else: 
+            fact = 1
+            n = num  # Guarda el valor original de num
+            while(num > 1): 
+                fact *= num 
+                num -= 1
+            print("Factorial ",n,"! es ", fact)  # Imprime el valor original de num
 
-if len(sys.argv) <= 1:
-   print("Debe informar un número!")
+if len(sys.argv) <= 2:
+   print("Debe informar un rango de números!")
    sys.exit()
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
+
+desde = int(sys.argv[1])
+hasta = int(sys.argv[2])
+
+factorial(desde, hasta)
 
