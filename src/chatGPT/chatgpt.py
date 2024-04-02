139faeef7907350ae4ef45e006a32d4140bba1e8
Lorenzo Paredes <lorenzoparedes122@gmail.com>
2024-04-02 19:52:39 -0300

    Agregando historial de consultas a chatgpt.py

--- src/chatGPT/chatgpt.py
+++ src/chatGPT/chatgpt.py
@@ -5,31 +5,42 @@ import openai
 openai.api_key = "api_key" 
 
 try:
-    userquery = input("Ingrese su consulta: ")
-
-    if not userquery.strip():
-        raise ValueError("Por favor, ingrese una consulta válida.")  
     
-    # Se realiza la consulta a través de la API de ChatGPT
-    response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo-0125",
-    messages=[
-    {
-    # Consulta del usuario
-    "role": "user",
-    "content": userquery }
-    ],
+    historial = []
     
-    temperature=1,
-    max_tokens=1000,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-    )
+    while True:
     
-    # Se muestra la conversacion entre usuario y chatGPT
-    print("You:", userquery)
-    print("chatGPT:", response.choices[0].message.content)
+        userquery = input("Ingrese su consulta: ")
+        
+        if userquery.strip(): # Se agrega la consulta al historial
+            historial.append(userquery)
+            
+        elif historial: # Si el historial no esta vacio, se puede acceder a la ultima consulta
+            userquery = historial[-1]
+            
+        else:
+            raise ValueError("Por favor, ingrese una consulta válida.")  
+        
+        
+        # Se realiza la consulta a través de la API de ChatGPT
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[
+        {
+        # Consulta del usuario
+        "role": "user",
+        "content": userquery }
+        ],
+        temperature=1,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+        )
+        
+        # Se muestra la conversacion entre usuario y chatGPT
+        print("You:", userquery)
+        print("chatGPT:", response.choices[0].message.content)
     
 except ValueError as ve:
     print(f"Error: {ve}")
