1370da9d863f54c2284b1a0d088af89c12bef0cb
Lorenzo Paredes <lorenzoparedes122@gmail.com>
2024-04-03 19:35:38 -0300

    Utilizando estrategias para reducir 'cyclomatic_complexity' en chatgpt.py

--- src/chatGPT/chatgpt.py
+++ src/chatGPT/chatgpt.py
@@ -5,10 +5,13 @@ import openai, sys # Se importan los paquetes necesarios para el programa
 openai.api_key = "api_key"
 
 def obtener_consulta():
+    
     userquery = input("Ingrese su consulta: ")
     return userquery.strip() # Retorna la consulta
 
+
 def realizar_consulta(userquery):
+    
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-0125", # Modelo de lenguaje utilizado
         messages=[{"role": "user", "content": userquery}], # Consulta del usuario
@@ -20,12 +23,15 @@ def realizar_consulta(userquery):
     )
     return response.choices[0].message.content # Retorna la respuesta de chatGPT
 
+
 def main():
     historial = [] # Almacena consultas
     buffer = [] # Almacena consultas y respuestas
 
+
     if "--convers" in sys.argv: # Verifica si se quiere acceder al modo de conversación
         print("--Modo de conversación activado--")
+
 
     while True:
         try:
@@ -34,16 +40,17 @@ def main():
             respuesta = realizar_consulta(userquery)
             buffer.append(userquery)
             buffer.append(respuesta)
-            
+           
             # Imprime la conversación entre el usuario y chatGPT
             print("You:", userquery)
             print("chatGPT:", respuesta)
-            
+           
         except ValueError as ve: # Manejo de excepción para consultas inválidas
             print(f"Error: {ve}")
-            
+           
         except Exception as e: # Manejo de excepción para otros errores
-            print(f"Error inesperado: {e}") 
+            print(f"Error inesperado: {e}")
+
 
 if __name__ == "__main__":
     main() # Este bloque se ejecuta cuando el archivo se ejecuta como programa principal
