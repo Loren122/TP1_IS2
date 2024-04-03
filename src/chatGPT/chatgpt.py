546454adbb5674cc14e1a1552e42d77111af84bb
Lorenzo Paredes <lorenzoparedes122@gmail.com>
2024-04-03 20:14:32 -0300

    Mejorando la comprension de chatgpt.py por medio de pylint

--- src/chatGPT/chatgpt.py
+++ src/chatGPT/chatgpt.py
@@ -1,17 +1,28 @@
-import openai, sys # Se importan los paquetes necesarios para el programa
+"""
+Se importan los archivos necesarios para el programa.
+"""
+import sys
+import openai
 
 
 # Se rellena con la API key
 openai.api_key = "api_key"
 
 def obtener_consulta():
-    
+    """"
+    Esta función obtiene una consulta del usuario a través de la entrada estándar.
+    Retorna la consulta después de eliminar los espacios en blanco al principio y al final.
+    """
     userquery = input("Ingrese su consulta: ")
     return userquery.strip() # Retorna la consulta
 
 
 def realizar_consulta(userquery):
-    
+    """
+    Esta función realiza una consulta utilizando el modelo de lenguaje GPT-3.5 Turbo.
+    Toma la consulta del usuario como argumento (userquery).
+    Retorna la respuesta generada por el modelo de chatGPT.
+    """
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-0125", # Modelo de lenguaje utilizado
         messages=[{"role": "user", "content": userquery}], # Consulta del usuario
@@ -25,6 +36,10 @@ def realizar_consulta(userquery):
 
 
 def main():
+    """
+    La función principal del programa.
+    Inicializa las listas historial y buffer para almacenar consultas y respuestas.
+    """
     historial = [] # Almacena consultas
     buffer = [] # Almacena consultas y respuestas
 
@@ -40,17 +55,13 @@ def main():
             respuesta = realizar_consulta(userquery)
             buffer.append(userquery)
             buffer.append(respuesta)
-           
             # Imprime la conversación entre el usuario y chatGPT
             print("You:", userquery)
             print("chatGPT:", respuesta)
-           
         except ValueError as ve: # Manejo de excepción para consultas inválidas
             print(f"Error: {ve}")
-           
         except Exception as e: # Manejo de excepción para otros errores
             print(f"Error inesperado: {e}")
 
-
-if __name__ == "__main__":
-    main() # Este bloque se ejecuta cuando el archivo se ejecuta como programa principal
+if __name__ == "__main__": # Se ejecuta cuando el archivo se ejecuta como programa principal
+    main()
